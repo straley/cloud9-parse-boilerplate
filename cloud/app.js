ccf1e5f5bc9df135fb64a497fe93a9adafea1b95
straley <straley@yahoo.com>
2013-09-27 23:11:38 -0400

    added comments

--- cloud/app.js
+++ cloud/app.js
@@ -1,23 +1,62 @@
-
-// These two lines are required to initialize Express in Cloud Code.
+/**
+ * Initialize Express
+ */
+ 
 var express = require('express');
 var app = express();
 
-var cloudPath = "cloud/";
+/**
+ * Set Up Express
+ */
+ 
+app.set('view engine', 'ejs');    // set EJS as the view engine 
+app.set('views', 'cloud/views');  // set the EJS folder
+app.use(express.bodyParser());    // set middleware
 
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+/**
+ * Create Routes
+ */
 
 app.get('/', function(req, res) {
-    var query = new Parse.Query("Counter");
-    query.equalTo("attr", "pageviews");
-    query.find({
-        success: function(results) {
-            res.render('hello', {message:JSON.stringify(results)});
-        }
-    })
+    counter("pageviews", 1, function(count){
+        res.render('hello', {pageviews:count});
+    });
 });
+
+/**
+ * Increment or decrement Counter attribute, pass new value to callback
+ * 
+ * @param (String) attribute name
+ * @param (Number) amount to increment (or decrement)
+ * @param (Function) fn (new_value)
+ * 
+ */
+ 
+var counter = function(attr, increment, fn) {
+    var query = new Parse.Query("Counter");
+    query.equalTo("attr", attr);
+    query.first({
+        success: function(result) {
+            var count = 0;
+            
+            if (!result) {
+                // create counter attribute-value and set to increment
+                // if it doesn't exist
+                var obj = Parse.Object("Counter");
+                console.log("saving Counter");
+                obj.save({"attr":attr, "value":increment});
+                count = increment;
+            } else {
+                // increase (or decrease) counter attribute-value by increment
+                result.increment("value", increment);   // increment the value
+                count = result.get("value");            // get the new value
+                result.save({"value":count});           // save the new value
+            }
+            
+            // callback fn with new count
+            if (typeof fn === "function") fn(count);
+        }
+    });
+};
 
 app.listen();
